c552753ccbddf539ce50a8600fd5a4c44b4c1a56
muba <m_kamzayev@kbtu.kz>
2024-11-15 00:52:48 +0500

    Initial Commit

---
+++ assignment8/Model/listOfFilm.swift
@@ -0,0 +1,14 @@
+//
+//  listOfFilm.swift
+//  assignment8
+//
+//  Created by Kaisar Kassymkhanov on 11.11.2024.
+//
+
+struct Films{
+    let title: String
+    let image: String
+    let description: String
+    let rating: Double
+    var favorite: Bool
+}
